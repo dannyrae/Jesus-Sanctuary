8b43c066dd989bf063a73206587c072240f885e4
Asuquo Emmanuel <86672860+dannyrae@users.noreply.github.com>
2022-04-15 10:54:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-openNav = ()=>{
-	let list = document.getElementById("myNav");
-	let tribe = document.getElementById("main");
+// openNav = ()=>{
+// 	let list = document.getElementById("myNav");
+// 	let tribe = document.getElementById("main");
 
-	if (list.style.display === "block") {
-		list.style.display = "none";
-		tribe.style.display = "flex";
-	} else {
-		list.style.display = "block";
-		tribe.style.display = "none";
-	}
+// 	if (list.style.display === "block") {
+// 		list.style.display = "none";
+// 		tribe.style.display = "flex";
+// 	} else {
+// 		list.style.display = "block";
+// 		tribe.style.display = "none";
+// 	}
+// }
+
+// $('.ham-menu').click(function(){
+// 	$(this).toggleClass("active");
+// 	$(".nav").toggleClass('active');
+// });
+
+function toggleSidebar(){
+	document.getElementById("sidebar").classList.toggle('active');
 }
